b168f4ab217f06cb0758dc775455f410cee9a93c
HenokKifleyesus <64043964+HenokKifleyesus@users.noreply.github.com>
2021-06-27 02:01:56 -0500

    sf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './component/user-list/user-list.component';
+import { UserFormComponent } from './component/user-form/user-form.component';
+
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'adduser', component: UserFormComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
